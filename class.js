d7754ba4363c28d71d70062fdbc76c544e274553
Jeysson <alyconr@hotmail.com>
2022-01-26 18:35:56 -0500

    class.js is added withh feature-2

--- class.js
+++ class.js
@@ -5,3 +5,19 @@ var Foo = class {
       return "Hello World!";
     }
   };
+
+//CLASE B 
+var instance = new claseb();
+instance.bar(); // "Hello World!"
+claseb.name; // 
+
+var claseb = class NamedClaseb {
+  constructor() {}
+  whoIsThere() {
+    return NamedClaseb.name;
+  }
+}
+var bar = new claseb();
+bar.whoIsThere(); 
+NamedClaseb.name; 
+claseb.name; 
