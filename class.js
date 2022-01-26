019ebdc3377683a54e8bcc1ea73cde648a7fcc44
Jeysson <alyconr@hotmail.com>
2022-01-26 18:38:21 -0500

    class.js is added withh feature-3

--- class.js
+++ class.js
@@ -5,3 +5,16 @@ var Foo = class {
       return "Hello World!";
     }
   };
+
+  //CLASE C
+
+var Clasec = class NamedClasec {
+    constructor() {}
+    whoIsThere() {
+      return NamedClasec.name;
+    }
+  }
+  var bar = new Clasec();
+  bar.whoIsThere(); // 
+  NamedClasec.name; // 
+  Clasec.name; // 
